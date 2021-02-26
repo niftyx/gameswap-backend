0724f4f8dc8cedf0da49b6db72d81530602ad65c
Jikun Liao <liaojikundever@gmail.com>
2021-02-26 07:18:27 -0600

    remove unnecessary parts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,8 @@
-import {
-  AssetSwapperContractAddresses,
-  ContractAddresses,
-} from "@0x/asset-swapper";
-import { getContractAddressesForChainOrThrow } from "./custom/contract-addresses";
 import * as express from "express";
 import { Server } from "http";
 import { Connection } from "typeorm";
 
 import {
-  CHAIN_ID,
   CONTENT_SECRET_KEY,
   LOGGER_INCLUDE_TIMESTAMP,
   LOG_LEVEL,
@@ -16,7 +10,7 @@ import {
 import { getDBConnectionAsync } from "./db_connection";
 
 import { runHttpServiceAsync } from "./runners/http_service_runner";
-import { ChainId, HttpServiceConfig } from "./types";
+import { HttpServiceConfig } from "./types";
 import * as pino from "pino";
 import { CollectionService } from "./services/collection_service";
 import { CryptoContentService } from "./services/crypto_content_service";
@@ -36,7 +30,6 @@ export const logger = pino({
 });
 
 export interface AppDependencies {
-  contractAddresses: ContractAddresses;
   connection: Connection;
   cryptoContentService: CryptoContentService;
   factoryService: FactoryService;
@@ -49,47 +42,6 @@ export interface AppDependencies {
   orderService: OrderService;
 }
 
-let contractAddresses_: AssetSwapperContractAddresses | undefined;
-
-/**
- * Determines the contract addresses needed for the network. For testing (ganache)
- * required contracts are deployed
- * @param provider provider to the network, used for ganache deployment
- * @param chainId the network chain id
- */
-export async function getContractAddressesForNetworkOrThrowAsync(
-  chainId: ChainId
-): Promise<AssetSwapperContractAddresses> {
-  if (contractAddresses_) {
-    return contractAddresses_;
-  }
-  let contractAddresses = getContractAddressesForChainOrThrow(chainId);
-  const bridgeAddresses = {
-    uniswapBridge: exports.NULL_ADDRESS,
-    uniswapV2Bridge: exports.NULL_ADDRESS,
-    eth2DaiBridge: exports.NULL_ADDRESS,
-    kyberBridge: exports.NULL_ADDRESS,
-    curveBridge: exports.NULL_ADDRESS,
-    multiBridge: exports.NULL_ADDRESS,
-    balancerBridge: exports.NULL_ADDRESS,
-    bancorBridge: exports.NULL_ADDRESS,
-    mStableBridge: exports.NULL_ADDRESS,
-    mooniswapBridge: exports.NULL_ADDRESS,
-    sushiswapBridge: exports.NULL_ADDRESS,
-    shellBridge: exports.NULL_ADDRESS,
-    dodoBridge: exports.NULL_ADDRESS,
-    creamBridge: exports.NULL_ADDRESS,
-    snowswapBridge: exports.NULL_ADDRESS,
-    swerveBridge: exports.NULL_ADDRESS,
-    cryptoComBridge: exports.NULL_ADDRESS,
-    positiveSlippageFeeTransformer: exports.NULL_ADDRESS,
-  };
-  // In a testnet where the environment does not support overrides
-  // so we deploy the latest sampler
-  contractAddresses_ = { ...contractAddresses, ...bridgeAddresses };
-  return contractAddresses_;
-}
-
 /**
  * Instantiates dependencies required to run the app. Uses default settings based on config
  * @param _config should contain a URI for mesh to listen to, and the ethereum RPC URL
@@ -97,9 +49,6 @@ export async function getContractAddressesForNetworkOrThrowAsync(
 export async function getDefaultAppDependenciesAsync(
   _config: HttpServiceConfig
 ): Promise<AppDependencies> {
-  const contractAddresses = await getContractAddressesForNetworkOrThrowAsync(
-    CHAIN_ID
-  );
   const connection = await getDBConnectionAsync();
 
   const collectionService = new CollectionService(connection);
@@ -124,7 +73,6 @@ export async function getDefaultAppDependenciesAsync(
   );
 
   return {
-    contractAddresses,
     connection,
     collectionService,
     collectionHistoryService,
